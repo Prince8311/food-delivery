cfa81dedefa49a2389a1c316d7b60a1334415c0f
Prince8311 <sourishmondal.vizac@gmail.com>
2025-04-14 01:03:57 +0530

    style design fix

---
+++ js/checkout-map-search.js
@@ -0,0 +1,149 @@
+window.onload = function () {
+    var defaultLatLng = { lat: 15.3173, lng: 75.7139 };
+    var map = new google.maps.Map(document.getElementById('dvMap'), {
+      center: defaultLatLng,
+      zoom: 20
+    });
+
+    var input = document.getElementById('searchInput');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    var infowindow = new google.maps.InfoWindow();
+    var geocoder = new google.maps.Geocoder();
+
+    var marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29),
+        draggable: true,
+        visible: false
+    });
+
+    let city = '', state = '', pincode = '';
+
+    function updateAddressFromLatLng(latlng) {
+        geocoder.geocode({ location: latlng }, function (results, status) {
+            if (status === 'OK') {
+                if (results[0]) {
+                    input.value = results[0].formatted_address;
+                    infowindow.setContent(results[0].formatted_address);
+                    infowindow.open(map, marker);
+                    // console.log(results[0]);
+                    const addressComponents = results[0].address_components;
+                    for (let i = 0; i < addressComponents.length; i++) {
+                        const types = addressComponents[i].types;
+                        if (types.includes('locality')) {
+                            city = addressComponents[i].long_name;
+                        }
+                        if (types.includes('administrative_area_level_1')) {
+                            state = addressComponents[i].long_name;
+                        }
+                        if (types.includes('postal_code')) {
+                            pincode = addressComponents[i].long_name;
+                        }
+                    }
+                } else {
+                    console.warn('No address found');
+                }
+            } else {
+                console.error('Geocoder failed due to: ' + status);
+            }
+        });
+    }
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var userLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            map.setCenter(userLocation);
+            map.setZoom(20); 
+            marker.setPosition(userLocation);
+            marker.setVisible(true);
+            updateAddressFromLatLng(userLocation);
+        }, function () {
+            console.warn('Geolocation failed or denied. Using default location.');
+        });
+    } else {
+        console.warn('Browser does not support Geolocation');
+    }
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("Autocomplete's returned place contains no geometry");
+            return;
+        }
+  
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(18);
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+    
+        var address = '';
+        if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+    
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        infowindow.open(map, marker);
+    });
+
+    google.maps.event.addListener(marker, 'dragend', function () {
+        var newPosition = marker.getPosition();
+        map.setCenter(newPosition);
+        updateAddressFromLatLng({
+            lat: newPosition.lat(),
+            lng: newPosition.lng()
+        });
+    });
+
+    document.getElementById('detect_current_location').addEventListener("click", () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var userLocation = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                map.setCenter(userLocation);
+                map.setZoom(20); 
+                marker.setPosition(userLocation);
+                marker.setVisible(true);
+                updateAddressFromLatLng(userLocation);
+            }, function () {
+                console.warn('Geolocation failed or denied. Using default location.');
+            });
+        } else {
+            console.warn('Browser does not support Geolocation');
+        }
+    });
+
+    const checkoutAddressForm = document.querySelector("#checkout_address_form");
+    const addressFillupForm = document.querySelector("#complete-address-form form");
+
+    addressFillupForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        checkoutAddressForm.querySelector('input[name="location"]').value = input.value;
+        checkoutAddressForm.querySelector('input[name="house_no"]').value = addressFillupForm.querySelector('input[name="house_no"]').value;
+        checkoutAddressForm.querySelector('input[name="floor"]').value = addressFillupForm.querySelector('input[name="floor"]').value;
+        checkoutAddressForm.querySelector('input[name="area"]').value = addressFillupForm.querySelector('input[name="area"]').value;
+        checkoutAddressForm.querySelector('input[name="landmark"]').value = addressFillupForm.querySelector('input[name="landmark"]').value;
+        checkoutAddressForm.querySelector('input[name="pincode"]').value = pincode;
+        checkoutAddressForm.querySelector('input[name="city"]').value = city;
+        checkoutAddressForm.querySelector('input[name="state"]').value = state;
+
+        document.getElementById('location_popup').classList.remove("active");
+    });
+}
