923bcba14a39f8fd48bf9872919d1d557ee19c64
Joydeepbarik <joydeepbarik007@gmail.com>
2025-04-16 01:29:57 +0530

    update

--- js/nav.js
+++ js/nav.js
@@ -12,33 +12,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-    const toggleBtn = document.querySelector(".res_nav_toggle_btn");
-    const sideberPopup = document.querySelector(".sideber_popup_section");
+const footer = document.querySelector(".footer");
+const toggleIcon = document.querySelector(".show a i"); 
 
-    toggleBtn.addEventListener("click", ()=> {
-        sideberPopup.classList.toggle("active");
-    });
+toggleIcon.addEventListener("click", function () {
+    if (footer.style.display === "none") {
+        footer.style.display = "block";
+        toggleIcon.classList.remove("fa-plus");
+        toggleIcon.classList.add("fa-minus");
+    } else {
+        footer.style.display = "none";
+        toggleIcon.classList.remove("fa-minus"); 
+        toggleIcon.classList.add("fa-plus"); 
+    }
+});
 
-    const anroidPopup = document.querySelector(".anroid_popup_wrapper");
-    const popupClose = document.getElementById('app-download-modal-close-btn');
-    popupClose.addEventListener("click", () => {
-        anroidPopup.classList.add("active");
-    });
+const toggleBtn = document.querySelector(".res_nav_toggle_btn");
+const sideberPopup = document.querySelector(".sideber_popup_section");
+
+toggleBtn.addEventListener("click", ()=> {
+    sideberPopup.classList.toggle("active");
+});
 
 
-    const footer = document.querySelector(".footer");
-    const toggleIcon = document.querySelector(".show a i"); 
-        
-        // footer.style.display = "none";
 
-        toggleIcon.addEventListener("click", function () {
-            if (footer.style.display === "none") {
-                footer.style.display = "block";
-                toggleIcon.classList.remove("fa-plus");
-                toggleIcon.classList.add("fa-minus");
-            } else {
-                footer.style.display = "none";
-                toggleIcon.classList.remove("fa-minus"); 
-                toggleIcon.classList.add("fa-plus"); 
-            }
-        });
+
+    
